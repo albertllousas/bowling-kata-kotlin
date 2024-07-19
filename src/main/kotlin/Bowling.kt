5c0c7a227856a5833a308429edd9322b1d8d716a
Albert Llousas Ortiz <abert.ortizl@gmail.com>
2024-07-19 16:35:46 +0200

    Ping

--- src/main/kotlin/Bowling.kt
+++ src/main/kotlin/Bowling.kt
@@ -1,11 +1,7 @@
 class Bowling {
     companion object {
         fun score(game: String): Int {
-            return if(game == "1") {
-                1
-            } else {
-                0
-            }
+            return game.toInt()
         }
     }
 }
--- src/test/kotlin/BowlingTest.kt
+++ src/test/kotlin/BowlingTest.kt
@@ -10,6 +10,14 @@ class BowlingTest {
             Pair("0", 0),
             Pair("1", 1),
             Pair("2", 2),
+            Pair("3", 3),
+            Pair("4", 4),
+            Pair("5", 5),
+            Pair("6", 6),
+            Pair("7", 7),
+            Pair("8", 8),
+            Pair("9", 9),
+            Pair("X", 10)
         ).map { (game, expected) ->
             DynamicTest.dynamicTest("$game $expected") {
                 Bowling.score(game) shouldBe expected
