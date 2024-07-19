08b81e622da6cbf3cae1eb9654ffed9a3aecc0d0
David Cortes Cutillas <dcortes@Davids-MacBook-Air.local>
2024-07-19 16:31:38 +0200

    Case 1, and test for 2

--- src/main/kotlin/Bowling.kt
+++ src/main/kotlin/Bowling.kt
@@ -1,7 +1,11 @@
 class Bowling {
     companion object {
         fun score(game: String): Int {
-            return 0
+            return if(game == "1") {
+                1
+            } else {
+                0
+            }
         }
     }
 }
--- src/test/kotlin/BowlingTest.kt
+++ src/test/kotlin/BowlingTest.kt
@@ -1,16 +1,19 @@
 import io.kotest.matchers.shouldBe
-import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.TestFactory
 
-class BowlingTest{
+class BowlingTest {
 
-    @Test
-    fun `given game with 0 pins down should end with 0 points`(){
-        Bowling.score("0") shouldBe 0
-    }
+    @TestFactory
+    fun `given game with 0 pins down should end with 0 points`() =
+        listOf(
+            Pair("0", 0),
+            Pair("1", 1),
+            Pair("2", 2),
+        ).map { (game, expected) ->
+            DynamicTest.dynamicTest("$game $expected") {
+                Bowling.score(game) shouldBe expected
+            }
+        }
 
-    @Test
-    fun `given game with 1 pin down should end with 1 point`(){
-        Bowling.score("1") shouldBe 1
-    }
 }
