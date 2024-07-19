a955e295fa2e8b7c1289ebf98ad40d805a1e718f
Albert Llousas Ortiz <abert.ortizl@gmail.com>
2024-07-19 16:36:26 +0200

    Ping

--- src/test/kotlin/BowlingTest.kt
+++ src/test/kotlin/BowlingTest.kt
@@ -7,7 +7,7 @@ class BowlingTest {
     @TestFactory
     fun `given game with 0 pins down should end with 0 points`() =
         listOf(
-            Pair("0", 0),
+            Pair("-", 0),
             Pair("1", 1),
             Pair("2", 2),
             Pair("3", 3),
@@ -17,7 +17,6 @@ class BowlingTest {
             Pair("7", 7),
             Pair("8", 8),
             Pair("9", 9),
-            Pair("X", 10)
         ).map { (game, expected) ->
             DynamicTest.dynamicTest("$game $expected") {
                 Bowling.score(game) shouldBe expected
