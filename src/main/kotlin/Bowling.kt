5686ee965ddc358ee33fd7bd52dbe74cfd1c21d8
David Cortes Cutillas <dcortes@Davids-Air.home>
2024-07-19 22:19:31 +0200

    Spare added and test for strike with continuation

--- src/main/kotlin/Bowling.kt
+++ src/main/kotlin/Bowling.kt
@@ -3,18 +3,26 @@ class Bowling {
         fun score(game: String): Int {
             var count = 0
             val frames = game.split(" ")
+            var spare = false
+
             for (frame in frames) {
-                var currentFrame =  frame
-                while (currentFrame != "") {
-                    val pinsDown = currentFrame.first()
-                    count += if (pinsDown == 'X') {
-                        10
-                    } else if (pinsDown == '-') {
-                        0
-                    } else {
-                        pinsDown.digitToInt()
+                when {
+                    frame.contains("/") -> {
+                        spare = true
+                        count += 10
                     }
-                    currentFrame = currentFrame.drop(1)
+
+                    frame.contains("X") -> {
+                        count += 10
+                    }
+
+                    else -> {
+                        val digits = frame.replace("-", "0")
+                        val firstVal = (digits[0].digitToInt() * if (spare) 2 else 1)
+                        val secondVal = digits.getOrElse(1) { '0' }.digitToInt()
+                        count += firstVal + secondVal
+                        spare = false
+                    }
                 }
             }
             return count
--- src/test/kotlin/BowlingTest.kt
+++ src/test/kotlin/BowlingTest.kt
@@ -25,7 +25,9 @@ class BowlingTest {
             Pair("22 22", 8),
             Pair("2- 33", 8),
             Pair("9- 9- 9- 9- 9- 9- 9- 9- 9- 9-", 90),
-            Pair("2/ 33", 16),
+            Pair("2/ 33", 19),
+            Pair("-/ 33", 19),
+            Pair("X 33", 22),
         ).map { (game, expected) ->
             dynamicTest("$game $expected") {
                 Bowling.score(game) shouldBe expected
