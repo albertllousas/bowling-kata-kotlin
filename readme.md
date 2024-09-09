a2fba74882a80d6025845716fe8d3e174579ca39
Albert Llousas Ortiz <abert.ortizl@gmail.com>
2024-09-09 08:18:31 +0200

    Restart

---
+++ readme.md
@@ -0,0 +1,2 @@
+# Bowling kata in kotlin
+
--- src/main/kotlin/Bowling.kt
+++ src/main/kotlin/Bowling.kt
@@ -1,32 +1,7 @@
 class Bowling {
     companion object {
         fun score(game: String): Int {
-            var count = 0
-            val frames = game.split(" ")
-            var spare = false
-
-            for (frame in frames) {
-                when {
-                    frame.contains("/") -> {
-                        spare = true
-                        count += 10
-                    }
-
-                    frame.contains("X") -> {
-                        count += 10
-                    }
-
-                    else -> {
-                        val digits = frame.replace("-", "0")
-                        val firstVal = (digits[0].digitToInt() * if (spare) 2 else 1)
-                        val secondVal = digits.getOrElse(1) { '0' }.digitToInt()
-                        count += firstVal + secondVal
-                        spare = false
-                    }
-                }
-            }
-            return count
+            TODO()
         }
-
     }
 }
