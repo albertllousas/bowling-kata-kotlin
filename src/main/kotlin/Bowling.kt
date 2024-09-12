9057ca4e532b2b401bf8774bdfcc0310c051ed5f
Albert Llousas Ortiz <abert.ortizl@gmail.com>
2024-09-12 16:41:33 +0200

    Small refactor

--- src/main/kotlin/Bowling.kt
+++ src/main/kotlin/Bowling.kt
@@ -7,16 +7,16 @@ class Bowling {
             var total = 0
             val frames = game.split(" ")
             frames.forEach { frame ->
-                total = scoreSingleFrame(frame, total)
+                total += scoreSingleFrame(frame)
             }
             return total
         }
 
-        private fun scoreSingleFrame(game: String, total: Int): Int {
-            var total1 = total
-            if (game[0].digitToIntOrNull() != null) total1 += game[0].digitToInt()
-            if (game.length > 1 && game[1].digitToIntOrNull() != null) total1 += game[1].digitToInt()
-            return total1
+        private fun scoreSingleFrame(game: String): Int {
+            var frameScore = 0
+            if (game[0].digitToIntOrNull() != null) frameScore += game[0].digitToInt()
+            if (game.length > 1 && game[1].digitToIntOrNull() != null) frameScore += game[1].digitToInt()
+            return frameScore
         }
     }
 }
